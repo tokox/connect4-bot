9f5165477a2a83375174cec6fdb2eb120f3a68ed
tokox <toko123456789@yahoo.com>
2023-06-04 17:40:29 +0200

    minimax simplification

--- minimax.cpp
+++ minimax.cpp
@@ -7,49 +7,18 @@ std::pair<int, int> minimax(Board& board, int depth, bool Max, int d) {
 	for(int i = 0; i < board.WIDTH; i++) {
 		if(board.empty[i]) {
 			board.move(i, Max?'O':'X');
-			int ev = eval(board);
-			if(ev == 1) {
-				board.unmove();
-				if(Max)
-					return {1, i};
-				else
-					return {-1, i};
-			}
-			if(d < depth) {
+			int ev = (Max?1:-1)*eval(board);
+			if(d < depth && ev == 0)
 				ev = minimax(board, depth, !Max, d+1).first;
-				if(Max) {
-					if(ev == 1) {
-						board.unmove();
-						return {1, i};
-					}
-				} else {
-					if(ev == -1) {
-						board.unmove();
-						return {-1, i};
-					}
-				}
-				if(Max) {
-					if(ev >= bestev) {
-						bestev = ev;
-						best = i;
-					}
-				} else {
-					if(ev <= bestev) {
-						bestev = ev;
-						best = i;
-					}
+			if(Max) {
+				if(ev >= bestev) {
+					bestev = ev;
+					best = i;
 				}
 			} else {
-				if(Max) {
-					if(ev >= bestev) {
-						bestev = ev;
-						best = i;
-					}
-				} else {
-					if(ev <= bestev) {
-						bestev = ev;
-						best = i;
-					}
+				if(ev <= bestev) {
+					bestev = ev;
+					best = i;
 				}
 			}
 			board.unmove();
